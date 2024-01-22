f370d69ce8eb34bee690d2fdb3d91eef076932b7
Néstor Iriondo <nestoririondo@gmail.com>
2024-01-22 21:16:23 +0100

    select all tiles to win the game

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./views/Home";
+import "./App.css"
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route index element={<Home />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
